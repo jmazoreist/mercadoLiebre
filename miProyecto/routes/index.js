070918294fe713f1500c61c9c2be27c1f17dbc56
jmazoreist <joaquin_mr@outlook.com>
2020-09-07 15:42:12 -0300

    Entrega inidividual de MercadoLiebre

---
+++ miProyecto/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/home', function(req, res, next){
+  res.render('home');
+});
+
+router.get('/registro', function(req, res, next){
+  res.render('registro');
+});
+
+router.get('/login', function(req, res, next){
+  res.render('login');
+});
+
+
+module.exports = router;
